eaa2622d9a09edadbaeb1d230860a70ceb3b8709
Craig Quiter <cquiter@gmail.com>
2018-02-02 15:47:16 -0700

    Remove query string from downloaded weights filename

--- camera_config.py
+++ camera_config.py
@@ -6,7 +6,27 @@ log = logs.get_log(__name__)
 # First dimension is rotated through at the end of the episode. Second dimension is for separate cameras on the car.
 rigs = {
     'baseline_rigs': [
-        [config.DEFAULT_CAM],
+        [
+            config.DEFAULT_CAM,
+            dict(name='forward_cam_wide_90', field_of_view=90, capture_width=340, capture_height=227,
+                 relative_position=[150, 1.0, 200],
+                 relative_rotation=[0.0, 0.0, 0.0]),
+            dict(name='semi_tall_cam_wide', field_of_view=110, capture_width=340, capture_height=227,
+                 relative_position=[150, 1.0, 400],
+                 relative_rotation=[0.0, -15.0, 0.0]),
+            dict(name='left_cam', field_of_view=60, capture_width=512, capture_height=289,
+                 relative_position=[150, -150., 200],
+                 relative_rotation=[0.0, 0.0, 0.0]),
+            dict(name='right_cam', field_of_view=60, capture_width=512, capture_height=289,
+                 relative_position=[150, 150., 200],
+                 relative_rotation=[0.0, 0.0, 0.0]),
+            dict(name='reverse_right_cam', field_of_view=60, capture_width=227, capture_height=227,
+                 relative_position=[150, 150., 200],
+                 relative_rotation=[0.0, 0.0, 180]),
+            dict(name='reverse_left_cam', field_of_view=60, capture_width=227, capture_height=227,
+                 relative_position=[150, -150., 200],
+                 relative_rotation=[0.0, 0.0, 180])
+        ],
         [dict(name='forward_cam_wide_90', field_of_view=90, capture_width=340, capture_height=227,
               relative_position=[150, 1.0, 200],
               relative_rotation=[0.0, 0.0, 0.0])],
