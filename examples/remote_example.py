b211a036e8112bdb3d879748a1d53c7e207fbe4b
Craig Quiter <cquiter@gmail.com>
2018-07-16 22:08:58 -0700

    Remote example instructions

--- examples/remote_example.py
+++ examples/remote_example.py
@@ -1,7 +1,7 @@
-
 import deepdrive
 from gym_deepdrive.envs.deepdrive_gym_env import Action
 
+# Usage: Please make sure to start a fresh api/server.py as connection handling is not implemented yet
 
 def main():
     env = deepdrive.start(is_remote_client=True, render=True)
