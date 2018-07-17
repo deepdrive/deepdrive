5ca2191af65a5a01440288fbdbb8e71464c88075
Craig Quiter <cquiter@gmail.com>
2018-07-16 22:14:33 -0700

    handling=>life cycle comment change

--- examples/remote_example.py
+++ examples/remote_example.py
@@ -1,7 +1,7 @@
 import deepdrive
 from gym_deepdrive.envs.deepdrive_gym_env import Action
 
-# Usage: Please make sure to start a fresh api/server.py as connection handling is not implemented yet
+# Usage: Please make sure to start a fresh api/server.py as connection life cycle is not properly implemented yet
 
 def main():
     env = deepdrive.start(is_remote_client=True, render=True)
