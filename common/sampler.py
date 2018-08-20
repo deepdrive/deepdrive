697e51bc0ce4aa6f3258bc869e9bc235459dd28c
Craig Quiter <cquiter@gmail.com>
2018-08-20 14:26:22 -0700

    Better sampler

--- common/sampler.py
+++ common/sampler.py
@@ -1,6 +1,7 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
+import random
 import time
 from random import randint
 
@@ -12,8 +13,10 @@ from collections import deque
 import numpy as np
 
 class Sampler(object):
-    """Simple data structure that maintains a fixed sized random sample of an input stream for computing mean, etc..."""
-    def __init__(self, hz=5, maxlen=100):
+    """
+    Simple data structure that maintains a fixed sized random sample of an input stream for computing mean, etc...
+    """
+    def __init__(self, hz=5, maxlen=10000):
         """
         hz: Frequency at which to collect samples
         maxlen: Number of samples to maintain
@@ -24,13 +27,20 @@ class Sampler(object):
         self.maxlen = maxlen
         self.last_sample_time = None
         self.is_full = False
+        self.num_samples = 0
 
-    def _add(self, x, index=None):
-        if index is None:
+    def _add(self, x):
+        self.num_samples += 1
+        if not self.is_full:
             self.q.append(x)
         else:
-            self.q.insert(index, x)
-        self.is_full = len(self.q) == self.maxlen
+            should_replace = self.is_full and random.random() < (1. / self.num_samples ** 0.85)
+            if should_replace:
+                index = randint(0, self.maxlen-1)
+                del self.q[index]
+                self.q.append(x)
+        if not self.is_full:
+            self.is_full = len(self.q) == self.maxlen
 
     def sample(self, x):
         if self.last_sample_time is None:
@@ -39,26 +49,17 @@ class Sampler(object):
             now = time.time()
             delta = now - self.last_sample_time()
             if delta >= self.period:
-                if not self.is_full:
-                    self._add(x)
-                else:
-                    index = randint(0, self.maxlen)
-                    self._add(x, index)
+                self._add(x)
 
     def mean(self):
-        return running_mean(self.q, len(self.q))[0]
-
-
-def running_mean(x, N):
-    # https://stackoverflow.com/a/27681394/134077
-    cumsum = np.cumsum(np.insert(x, 0, 0))
-    return (cumsum[N:] - cumsum[:-N]) / float(N)
+        return np.mean(self.q)
 
 
 if __name__ == '__main__':
-    s = Sampler(hz=10**10, maxlen=1000)
-    for i in range(1000):
+    s = Sampler(hz=10**10, maxlen=5)
+    for i in range(10000):
         start = time.time()
-        s.sample(randint(0, 100))
+        # s.sample(randint(0, 100))
+        s.sample(i)
         mean = s.mean()
-        print(mean, 'took', time.time() - start, 's')
+        print(mean, 'took', time.time() - start, 's', s.q)
