451a60bda2b641201901b1d04ab4102f51945a82
Craig Quiter <cquiter@gmail.com>
2019-06-22 16:54:23 -0700

    Add timer "with" class

--- sim/world.py
+++ sim/world.py
@@ -1,6 +1,9 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
+import time
+from typing import List
+
 import numpy as np
 from future.builtins import (int, open, round,
                              str)
