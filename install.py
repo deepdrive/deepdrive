ee734943e061d25cb5ade2f4cdba670d30ba78f0
Craig Quiter <cquiter@gmail.com>
2019-09-23 16:34:57 -0700

    Bump deepdrive bindings version

--- install.py
+++ install.py
@@ -95,7 +95,7 @@ def main():
     # # TODO: Remove dev0 once 3.0 is stable
     # run_command_with_sarge(py + ' -m pip install {pip_args} "deepdrive > {major_minor_version}.*dev0"'.format(
 
-    run_command_with_sarge(py + ' -m pip install {pip_args} "deepdrive==3.0.20190625221105.dev0"'.format(
+    run_command_with_sarge(py + ' -m pip install {pip_args} "deepdrive==3.0.20190920222155.dev0"'.format(
         major_minor_version=c.MAJOR_MINOR_VERSION_STR, pip_args=pip_args))
 
     # noinspection PyUnresolvedReferences
