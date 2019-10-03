5f19a7a35526ad648970d4da0ca6480650b47e6a
Craig Quiter <cquiter@gmail.com>
2019-10-03 14:09:26 -0700

    Login to docker before building

--- .ci/build_botleague_containers.sh
+++ .ci/build_botleague_containers.sh
@@ -13,4 +13,5 @@ git checkout -qf ${DEEPDRIVE_COMMIT}
 # Build base container
 make
 
+cat <<< ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
 python3 -u .ci/build_botleague_containers.py
