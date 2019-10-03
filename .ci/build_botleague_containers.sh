abb8bc4b6eae06550087cd4a8bbf826df3ae9b79
Craig Quiter <cquiter@gmail.com>
2019-10-03 14:01:15 -0700

    Fix build directory

--- .ci/build_botleague_containers.sh
+++ .ci/build_botleague_containers.sh
@@ -5,10 +5,7 @@ set -u  # Attempt to use undefined variable outputs error message, and forces an
 set -x  # Similar to verbose mode (-v), but expands commands
 set -o pipefail  # Causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value.
 
-DIR=`dirname "$0"`
-cd ${DIR}/..
-
-cd deepdrive
+cd /deepdrive-build/deepdrive
 git clean -fxd
 git fetch origin ${DEEPDRIVE_BRANCH}
 git checkout -qf ${DEEPDRIVE_COMMIT}
