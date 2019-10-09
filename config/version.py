caddf902845e59f1501f7c01279d8306a2adec46
Craig Quiter <cquiter@gmail.com>
2019-10-09 16:35:48 -0700

    Remove python 2 aspirations

--- config/version.py
+++ config/version.py
@@ -1,12 +1,3 @@
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
-
-# TODO: Bootstrap future module to enable Python 2 support of install which depends on this file to do below
-# from future.builtins import (ascii, bytes, chr, dict, filter, hex, input,
-#                              int, map, next, oct, open, pow, range, round,
-#                              str, super, zip)
-
-
 from distutils.version import LooseVersion as semvar
 
 from config.directories import *
