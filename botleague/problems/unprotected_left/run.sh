f93e1091cdd9e393fd5516eedbf85e19e380773c
Craig Quiter <cquiter@gmail.com>
2019-11-12 15:49:10 -0800

    Update unprocted left with tuned values

--- botleague/problems/unprotected_left/run.sh
+++ botleague/problems/unprotected_left/run.sh
@@ -6,15 +6,14 @@ export DEEPDRIVE_PUBLIC=true
 
 python main.py \
       --server \
-      --path-follower \
-      --max-steps=500 \
+      --max-steps=1440 \
       --max-episodes=1 \
       --record \
       --eval-only \
-      --render \
-      --eval-only \
       --upload-gist \
-      --map=kevindale_bare \
       --scenario=2 \
+      --map=kevindale_bare \
+      --sync \
+      --camera-rigs=default_rig_1080p \
       --image-resize-dims="[224,224,3]" \
       && echo "Sim finished running successfully. Check above for results."
