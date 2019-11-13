8acb61d23d3d3d55a563221b3257a65a5c74d946
Craig Quiter <cquiter@gmail.com>
2019-11-13 10:38:53 -0800

    Update example to use new map

--- example.py
+++ example.py
@@ -2,8 +2,16 @@ import sim
 
 
 def main():
-    env = sim.start()
-    forward = sim.action(throttle=1, steering=0, brake=0)
+    env = sim.start(
+
+        # map can be canyons, kevindale, kevindale_bare, or jamestown
+        map='kevindale_bare',
+
+        # scenario can be 0 => 5 - Scenario descriptions:
+        # https://gist.github.com/crizCraig/855a5cc4bc96fc2765cb9bf5d6f953b4
+        scenario_index=1
+    )
+    forward = sim.action(throttle=0.75, steering=0, brake=0)
     done = False
     while not done:
         observation, reward, done, info = env.step(forward)
